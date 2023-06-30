ca2f127f172cb913dc21495e11161a7d72452d0f
Mhistersax <bamigboyepelumi38@gmail.com>
2023-06-30 14:49:53 +0100

    edit

---
+++ Java_learning_hub/src/Special.java
@@ -0,0 +1,18 @@
+/*
+ * solve the following using Java
+ * Test Data:
+a. -5 + 8 * 6
+b. (55+9) % 9
+c. 20 + -3*5 / 8
+d. 5 + 15 / 3 * 2 - 8 % 3
+Hints: BODMAS
+ */
+
+public class Special {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
